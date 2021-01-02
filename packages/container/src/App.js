1162b694e7a5b65d8f275b600aa8ab7ce87f0f4b
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-02 09:56:29 -0300

    chore commit lol

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hallo!</h1>
+      <h1>Hi There!</h1>
       <hr />
       <MarketingApp />
     </div>
