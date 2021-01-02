014c2e4262b262bbdc7e60e3fd058896f7537b18
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-02 09:48:09 -0300

    adicionando public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const config = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
