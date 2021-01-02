b5cfaad11f020db35223c6740d816e1067f7784f
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-01-02 08:22:12 -0300

    adicionando configuraçoes de prod

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commom = require("./webpack.common");
+const package = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const config = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: package.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commom, config);
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commom = require("./webpack.common");
+const package = require("../package.json");
+
+const config = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: package.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(config, commom);
